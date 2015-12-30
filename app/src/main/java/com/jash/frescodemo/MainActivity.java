940345e800fb60e3f3778c0d69781dbc9f72c7d0
cnso <hdmj@sina.com>
2015-12-30 11:24:11 +0800

    进度

--- app/src/main/java/com/jash/frescodemo/MainActivity.java
+++ app/src/main/java/com/jash/frescodemo/MainActivity.java
@@ -9,7 +9,14 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.controller.AbstractDraweeController;
+import com.facebook.drawee.drawable.ProgressBarDrawable;
+import com.facebook.drawee.generic.GenericDraweeHierarchy;
+import com.facebook.drawee.generic.GenericDraweeHierarchyBuilder;
 import com.facebook.drawee.view.SimpleDraweeView;
+import com.facebook.imagepipeline.request.ImageRequest;
+import com.facebook.imagepipeline.request.ImageRequestBuilder;
 
 import java.io.File;
 
@@ -22,16 +29,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         drawee = (SimpleDraweeView) findViewById(R.id.drawee);
+        GenericDraweeHierarchy build = new GenericDraweeHierarchyBuilder(getResources())
+                .setProgressBarImage(new ProgressBarDrawable()).build();
+        drawee.setHierarchy(build);
+        Uri uri = Uri.parse("http://wallpaperswide.com/download/captain_america_vs_iron_man-1440x1080.jpg");
+        ImageRequest request = ImageRequestBuilder
+                .newBuilderWithSource(uri)
+                .setProgressiveRenderingEnabled(true)
+                .build();
+        AbstractDraweeController controller = Fresco.newDraweeControllerBuilder()
+                .setImageRequest(request)
+                .setOldController(drawee.getController())
+                .build();
+
+        drawee.setController(controller);
 //        drawee.setImageURI(Uri.parse("https://material-design.storage.googleapis.com/publish/material_v_4/material_ext_publish/0Bx4BSt6jniD7NndTQW9VZTlZV2s/materialdesign_principles_bold.png"));
-        if (ActivityCompat.checkSelfPermission(
-                this, Manifest.permission.READ_EXTERNAL_STORAGE)
-                == PackageManager.PERMISSION_GRANTED) {
-            drawee.setImageURI(Uri.fromFile(
-                    new File(
-                            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "347.jpg")));
-        } else {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
-        }
+//        if (ActivityCompat.checkSelfPermission(
+//                this, Manifest.permission.READ_EXTERNAL_STORAGE)
+//                == PackageManager.PERMISSION_GRANTED) {
+//            drawee.setImageURI(Uri.fromFile(
+//                    new File(
+//                            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "347.jpg")));
+//        } else {
+//            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
+//        }
     }
 
     @Override
